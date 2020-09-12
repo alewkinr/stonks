5b2994be0267e4d5860e4ce165b40df2998cce25
MrMurdock11 <mambetov.azamat.11@yandex.ru>
2020-09-12 20:42:01 +0300

    Добавил scollbar для списка облигаций в портфеле.
    - Добавил scollbar для списка облигаций в портфеле.
    - Добавил зависимость с компонентом для построения пользовательских scrollbars.

--- src/pages/Home/childs/Sidebar/childs/Portfolio/Portfolio.view.tsx
+++ src/pages/Home/childs/Sidebar/childs/Portfolio/Portfolio.view.tsx
@@ -4,19 +4,21 @@ import React from "react";
 import { PortfolioProps } from "./Portfolio";
 import { InformationOnFunds } from "./childs/InformationOnFunds";
 import { Bond } from "./childs/Bond";
+import { Scrollbars } from "react-custom-scrollbars";
 
 export const PortfolioView: React.FC<PortfolioProps> = (props) => {
+	const value = "a".repeat(30);
+	const array = [...value];
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.label}>{"Портфель"}</div>
 			<InformationOnFunds />
-			<div className={styles.bonds}>
-				<Bond />
-				<Bond />
-				<Bond />
-				<Bond />
-				<Bond />
-			</div>
+			<Scrollbars autoHide style={{width: "100%", height: "420px"}}>
+				<div className={styles.bonds}>
+					{array.map(it => <Bond />)}
+				</div>
+			</Scrollbars>
 		</div>
 	);
 }
