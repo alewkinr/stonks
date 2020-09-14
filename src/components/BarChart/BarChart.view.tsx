a393738eb5be4fa005825ebda834bfc806e48eba
Northenfox <gordienko.hogart@gmail.com>
2020-09-14 10:43:01 +0300

    add charts to components and chart page

---
+++ src/components/BarChart/BarChart.view.tsx
@@ -0,0 +1,35 @@
+import styles from "./BarChart.style.css";
+
+import React from "react";
+import { BarChartProps } from "./BarChart";
+import { Label } from "../Label";
+
+import { Chart } from "react-google-charts";
+
+export const BarChartView: React.FC<BarChartProps> = (props) => {
+    const { data } = props;
+    return (
+        <div className={styles.container}>
+            <Chart
+                width={"700px"}
+                height={"300px"}
+                chartType="ColumnChart"
+                chartLanguage="ru"
+                loader={<div>Loading Chart</div>}
+                data={data}
+                options={{
+                    chart: {
+                        title: "Company Performance",
+                        subtitle: "Sales, Expenses, and Profit: 2014-2017",
+                        bar: { groupWidth: "95%" },
+                    },
+                    chartArea: { width: "80%" },
+                    isStacked: true,
+                    legend: { position: "top", maxLines: 5 },
+                    colors: ["#7A7285", "#00CC00"],
+                }}
+                rootProps={{ "data-testid": "2" }}
+            />
+        </div>
+    );
+};
--- src/pages/Home/childs/Workspace/childs/Charts/Charts.view.tsx
+++ src/pages/Home/childs/Workspace/childs/Charts/Charts.view.tsx
@@ -3,13 +3,18 @@ import styles from "./Charts.style.css";
 import React from "react";
 import { ChartsProps } from "./Charts";
 
+import { BarChart } from "../../../.././../../components/BarChart";
+
 export const ChartsView: React.FC<ChartsProps> = (props) => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.navbar}>
-				<div className={`${styles.item} ${styles.active}`}>{"Доходность"}</div>
-				<div className={styles.item}>{"Состав"}</div>
-			</div>
-		</div>
-	);
-}
+    return (
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <div className={`${styles.item} ${styles.active}`}>
+                    {"Доходность"}
+                </div>
+                <div className={styles.item}>{"Состав"}</div>
+            </div>
+            <BarChart />
+        </div>
+    );
+};
