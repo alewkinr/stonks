76e35462009e6cf3d80da7c5553463fb353d8022
Northenfox <gordienko.hogart@gmail.com>
2020-09-14 15:51:43 +0300

    add changes in interface

--- src/pages/Home/childs/Sidebar/childs/Portfolio/Portfolio.view.tsx
+++ src/pages/Home/childs/Sidebar/childs/Portfolio/Portfolio.view.tsx
@@ -7,18 +7,23 @@ import { Bond } from "./childs/Bond";
 import { Scrollbars } from "react-custom-scrollbars";
 
 export const PortfolioView: React.FC<PortfolioProps> = (props) => {
-	const value = "a".repeat(30);
-	const array = [...value];
+    const value = "a".repeat(30);
+    const array = [...value];
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.label}>{"Портфель"}</div>
-			<InformationOnFunds />
-			<Scrollbars autoHide style={{width: "100%", height: "420px"}}>
-				<div className={styles.bonds}>
-					{array.map(it => <Bond />)}
-				</div>
-			</Scrollbars>
-		</div>
-	);
-}
+    return (
+        <div className={styles.container}>
+            <div className={styles.containerRow}>
+                <div className={styles.label}>{"Портфель"}</div>
+                <div className={`${styles.control} ${styles.plus}`} />
+            </div>
+            <InformationOnFunds />
+            <Scrollbars autoHide style={{ width: "100%", height: "420px" }}>
+                <div className={styles.bonds}>
+                    {array.map((it) => (
+                        <Bond />
+                    ))}
+                </div>
+            </Scrollbars>
+        </div>
+    );
+};
