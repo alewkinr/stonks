13a8a840803d4d004ecd6687c151df04e5e4da84
Northenfox <gordienko.hogart@gmail.com>
2020-09-15 18:03:05 +0300

    changes to calendar && charts

--- src/pages/Home/childs/Workspace/childs/Charts/Charts.tsx
+++ src/pages/Home/childs/Workspace/childs/Charts/Charts.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { ChartsView } from "./Charts.view";
 
-type ChartsState = { /* your states */ };
+type ChartsState = {
+    onProfit?: boolean;
+};
 
 export type ChartsProps = ChartsState;
 
 export const Charts: React.FC<ChartsProps> = (props) => {
-	return <ChartsView {...props} />
-}
+    return <ChartsView {...props} />;
+};
+
+Charts.defaultProps = {
+    onProfit: true,
+};
--- src/pages/Home/childs/Workspace/childs/CouponCalendar/childs/Row/Row.view.tsx
+++ src/pages/Home/childs/Workspace/childs/CouponCalendar/childs/Row/Row.view.tsx
@@ -6,17 +6,15 @@ import { RowProps } from "./Row";
 import tinkoff from "./contents/tinkoff.png";
 
 export const RowView: React.FC<RowProps> = (props) => {
+    const { date, bondName, sum } = props;
     return (
         <div className={styles.container}>
-            <div>{"22.07.2021"}</div>
             <div className={styles.iconAndTitle}>
                 <img src={tinkoff} alt="row-icon" className={styles.image} />
-                <div className={styles.name}>{"Тинькофф1R"}</div>
+                <div className={styles.name}>{bondName}</div>
             </div>
-            <div className={styles.percent}>
-                <span>{"11,55"}</span>
-            </div>
-            <div className={styles.price}>{"1 103"}</div>
+            <div>{date}</div>
+            <div className={styles.price}>{sum}</div>
         </div>
     );
 };
