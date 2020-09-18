ec4593f549500942be49a25314925427bef29362
Ramil Aleshkin <alewkinr@gmail.com>
2020-09-18 01:44:29 +0000

    🔥 Removed legacy files

--- src/store/Projection/Projection.actions.type.ts
+++
@@ -1,5 +0,0 @@
-import * as ActionsFC from "./Projection.actions";
-
-type InferValueTypes<T> = T extends { [key: string]: infer U } ? U : never;
-
-export type Actions = ReturnType<InferValueTypes<typeof ActionsFC>>;
